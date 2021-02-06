696b6afe6ab5e801d789679639d348696093bad3
Vance Will <vancewilll@icloud.com>
2021-02-06 18:15:52 +0800

    add missing file

---
+++ Sources/Wormhole/TransitingType.swift
@@ -0,0 +1,16 @@
+//
+//  TransitingType.swift
+//
+//
+//  Created by Ethan Mengoreo on 2021/2/3.
+//
+
+import Foundation
+
+public enum TransitingType {
+    case file
+    case coordinatedFile
+    case sessionContext
+    case sessionMessage
+    case sessionFile
+}
