afb38c21b3c4a3b5d1e39a5e5d530848c73eab3a
Vance Will <vancewilll@icloud.com>
2021-02-07 00:56:08 +0800

    new example

---
+++ Example/Shared/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Shared
+//
+//  Created by Ethan Mengoreo on 2/7/21.
+//
+
+import SwiftUI
+import Wormhole
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Example/watchOS Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  watchOS Extension
+//
+//  Created by Ethan Mengoreo on 2/7/21.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
